5dbdf75e03372381f96bf6563d60a117b4a0952a
candyflossuk <ross.a.humphrey@googlemail.com>
2021-03-19 22:07:17 +0000

    Created a Django model for a blog post

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -13,3 +13,29 @@ class Post(models.Model):
         ("draft", "DRAFT"),
         ("published", "Published"),
     )
+
+    title = models.CharField(max_length=250)  # Translates to VARCHAR
+    slug = models.SlugField(
+        max_length=250, unique_for_date="publish"
+    )  # Used for URL, unique_for_date prevents duplicate slugs for posts with same publish date and slug
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )  # Many to One (one user can create many blog posts).
+    # on_delete - CASCADE will ensure all blog posts by a deleted user are deleted
+    body = models.TextField()  # Translates to TEXT
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(
+        auto_now=True
+    )  # auto_now_add - date is saved automatically when object is created
+    updated = models.DateTimeField(auto_now=True)
+    # auto_now - date is updated when saving an object
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default="draft"
+    )  # Can only be from the STATUS_CHOICES given
+
+    class Meta:
+        # Contains the metadata - sort results by the 'publish' field in descending order
+        ordering = ("-publish",)
+
+    def __str__(self):
+        return self.title
