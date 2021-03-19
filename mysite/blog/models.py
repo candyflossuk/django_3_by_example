340e9dbc5d39bad45bb3ad18e60a6a927bf0015c
candyflossuk <ross.a.humphrey@googlemail.com>
2021-03-19 21:56:12 +0000

    Added black as requirement

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here - each model will create a table
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+
+    STATUS_CHOICES = (
+        ("draft", "DRAFT"),
+        ("published", "Published"),
+    )
