6385d86dd07f35bd9d212ce1e6f76c0405ca243e
candyflossuk <ross.a.humphrey@googlemail.com>
2021-03-21 20:34:07 +0000

    Added two views- post_list to get all posts, post_detail to retrieve 1 post for a given slug on a given date

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+# Create your views here - a view is just Python function that receives a web req and returns a response
+# All logic to return desired response goes inside the view
+
+
+def post_list(request):  # request param required by all views
+    posts = Post.published.all()
+    # Use render() to render the list of posts with the template
+    # Takes request context into account (template context processors are callables that set variables into the context)
+    return render(request, "/blog/post/list.html", {"posts": posts})
+
+
+def post_detail(request, year, month, day, post):
+    # Retrieve a published post with the given slug and date
+    # Use get_object_or_404 as shortcut to retrieve desired post - if not match HTTP exception is returned
+    post = get_object_or_404(
+        Post,
+        slug=post,
+        status="published",
+        publish__year=year,
+        publish__month=month,
+        publish__date=day,
+    )
+    return render(request, "blog/post/detail.html", {"post": post})
