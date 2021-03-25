4343e6c09e1c86114a6749513ac7fa1bd89d5f89
candyflossuk <ross.a.humphrey@googlemail.com>
2021-03-25 09:02:16 +0000

    Added ability to send email via Django's inbuilt email service

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -73,6 +73,6 @@ def post_share(request, post_id):
             sent = True
     else:
         form = EmailPostForm()  # displays an empty form
-        return render(
-            request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
-        )
+    return render(
+        request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
+    )
