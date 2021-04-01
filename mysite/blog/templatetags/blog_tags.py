e000d03252ff658a412a52b3e0d2023e65b2a7d2
candyflossuk <ross.a.humphrey@googlemail.com>
2021-04-01 10:06:00 +0100

    Added most commented posts to the side bar

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 from ..models import Post
 
 """
@@ -25,3 +26,11 @@ def total_posts():
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by("-publish")[:count]
     return {"latest_posts": latest_posts}
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    # Build query set using annotate
+    return Post.published.annotate(total_comments=Count("comments")).order_by(
+        "-total_comments"
+    )[:count]
